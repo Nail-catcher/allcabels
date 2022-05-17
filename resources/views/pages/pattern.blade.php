1908c2a1a3e000decb80c8aa3f5312535402f1de
Nail-catcher <nikita-mokin@mail.ru>
2022-05-17 10:47:41 +0300

    flex

--- resources/views/pages/pattern.blade.php
+++ resources/views/pages/pattern.blade.php
@@ -36,7 +36,7 @@
                           <a title="Общие справочники" href="/patternotherguides/?pattern={{$pattern->id}}" class="table__action-exclusion"><img src="{{ asset('images/svg/book.svg') }}" alt=""></a>
 
                           <a title="Исключения" href="/patternconflicts/?pattern={{$pattern->id}}" class="table__action-exclusion"><img src="{{ asset('images/svg/exc.svg') }}" alt=""></a>
-                        <a title="Редактировать" href="#" class="table__action-edit"><img src="{{ asset('images/svg/edit.svg') }}" alt=""></a>
+                        <a title="Редактировать" href="/patterns/{{$pattern->id}}" class="table__action-edit"><img src="{{ asset('images/svg/edit.svg') }}" alt=""></a>
                         <a title="Удалить" onclick="return confirm('Are you sure?')" href="{{url('pattern/destroy/'.$pattern->id)}}"  class="table__action-remove"><img src="{{ asset('images/svg/trash.svg') }}" alt=""></a>
                       </div>
                     </td>
